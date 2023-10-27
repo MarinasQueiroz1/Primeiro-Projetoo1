d72d6217ddef86f6af5c67d9b98a6ba28aaae1f1
Pedrofarley7 <fulanodetal@exemplo.br>
2023-10-27 15:32:21 -0300

    Primeiro projeto novo

---
+++ Documents/NetBeansProjects/Projetoo1/src/main/java/ifce/projetoo1/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ifce.projetoo1;
+
+import modelo.Esteira;
+/**
+ *
+ * @author Alunos
+ */
+public class Principal {
+   
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Esteira e1 = new Esteira();
+        e1.ligar();
+        e1.desligar();
+    }
+    
+}
