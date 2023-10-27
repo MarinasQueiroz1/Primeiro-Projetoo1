e4cd7d0252f5e984fa14c84c19bce0d689182e6e
Pedrofarley7 <fulanodetal@exemplo.br>
2023-10-27 15:19:10 -0300

    Primeiro projeto novo

---
+++ Documents/NetBeansProjects/Projetoo1/src/main/java/modelo/Esteira.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Alunos
+ */
+public class Esteira {
+     //ATRIBUTOS
+    private boolean estado;
+    //CONSTRUTOR
+     public Esteira(){
+        estado = true;
+    }
+    //METODOS
+    public void ligar(){
+        estado = true;
+        JOptionPane.showMessageDialog(null,"Esteira Ligada");
+        
+    }
+    public void desligar(){
+        estado = false;
+        JOptionPane.showMessageDialog(null,"Esteira Ligada");
+    }
+    
+}
